ff7cdff2730b7b359d19631d3378b6e057fe2691
TABASSUM KHANUM <55125208+tabassum2507@users.noreply.github.com>
2022-10-10 16:31:12 +0530

    Update index.py

--- api/index.py
+++ api/index.py
@@ -1,15 +1,12 @@
 from http.server import BaseHTTPRequestHandler
-from urllib import parse
+from cowpy import cow
+
 class handler(BaseHTTPRequestHandler):
+
     def do_GET(self):
-        s = self.path
-        dic = dict(parse.parse_qsl(parse.urlsplit(s).query))
         self.send_response(200)
         self.send_header('Content-type','text/plain')
         self.end_headers()
-        if "name" in dic:
-            message = "Hello, " + dic["name"] + "!"
-        else:
-            message = "Hello, stranger!"
+        message = cow.Cowacter().milk('Hello from Python from a Serverless Function!')
         self.wfile.write(message.encode())
         return
